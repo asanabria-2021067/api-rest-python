3485476b630999147262de221bbcedbf5a4fcbb2
Angel Sanabria <asanabria-2021067@kinal.edu.gt>
2024-03-31 11:03:51 -0600

    Delete bson

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify, Response
 from flask_pymongo import PyMongo
-from bson import json_util, ObjectId
 from dotenv import load_dotenv
 from flask_cors import CORS
 import os
@@ -16,85 +15,83 @@ mongo = PyMongo(app)
 CORS(app)
 
 # HOME ROUTE
-@app.route('/' , methods = ['GET'])
+@app.route('/', methods=['GET'])
 def home():
     response = "BIENVENIDO AL BACKEND DE SEASOS"
-    return Response(response, mimetype='application/txt')
+    return Response(response, mimetype='text/plain')
 
 # POST ANIMALS
-@app.route('/post/animals' , methods = ['POST'])
+@app.route('/post/animals', methods=['POST'])
 def create_animal():
-    nombre = request.json['nombre']
-    cientifico = request.json['cientifico']
-    region = request.json['region']
-    latitud = request.json['latitud']
-    longitud = request.json['longitud']
-    img = request.json['img']
+    data = request.json
+    nombre = data.get('nombre')
+    cientifico = data.get('cientifico')
+    region = data.get('region')
+    latitud = data.get('latitud')
+    longitud = data.get('longitud')
+    img = data.get('img')
     status = False
 
     if nombre and cientifico and region and latitud and longitud:
         row = mongo.db.animals.insert_one({
-            "nombre" : nombre,
-            "cientifico" :cientifico,
-            "region" : region,
+            "nombre": nombre,
+            "cientifico": cientifico,
+            "region": region,
             "latitud": latitud,
             "longitud": longitud,
             "img": img,
             "status": status
         })
         response = {
-            'id': str(row),
+            'id': str(row.inserted_id),
             'nombre': nombre,
-            'cientifico':cientifico,
-            'region' : region,
+            'cientifico': cientifico,
+            'region': region,
             "latitud": latitud,
             "longitud": longitud,
             "img": img,
             "status": status
         }
-        return response
+        return jsonify(response)
     else:
         return not_found()
 
-    return{'message': 'received'}
-
 # GET ANIMALS
-@app.route('/get/animals', methods = ['GET'])
+@app.route('/get/animals', methods=['GET'])
 def get_animals():
     animals_list = mongo.db.animals.find({'status': True})
-    response = json_util.dumps(animals_list)
-    return Response(response, mimetype='application/json')
+    animals = [animal for animal in animals_list]
+    return jsonify(animals)
 
 # ANIMALS BY REGION OR NAME
-@app.route('/get/animals/<id>', methods = ['GET'])
+@app.route('/get/animals/<id>', methods=['GET'])
 def get_animal(id):
-    animals_list = mongo.db.animals.find_one(({'nombre': id} or {'region': id}) and {'status':True})
-    response = json_util.dumps(animals_list)
-    return Response(response, mimetype='application/json')
+    animals_list = mongo.db.animals.find({'$or': [{'nombre': id}, {'region': id}], 'status': True})
+    animals = [animal for animal in animals_list]
+    return jsonify(animals)
 
 # UPDATE ANIMALS (STATUS)
-@app.route('/update/animals/<id>', methods = ['PUT'])
+@app.route('/update/animals/<id>', methods=['PUT'])
 def update_animal(id):
-    data = request.get_json()
-    response = mongo.db.animals.update_one({'_id': ObjectId(id)}, {'$set': data})
-    if response.modified_count >=1:
-        return{'message': 'Animal update'}
+    data = request.json
+    response = mongo.db.animals.update_one({'_id': id}, {'$set': data})
+    if response.modified_count >= 1:
+        return jsonify({'message': 'Animal updated'})
     else:
-        not_found()
+        return not_found()
 
 # DELETE ANIMAL
 @app.route('/delete/animals/<id>', methods=['DELETE'])
 def delete_animal(id):
-    deleted_animal = mongo.db.animals.find_one_and_delete({'_id': ObjectId(id)})
-    
+    deleted_animal = mongo.db.animals.find_one_and_delete({'_id': id})
     if deleted_animal:
-        return {'message': 'Animal deleted'}
+        return jsonify({'message': 'Animal deleted'})
     else:
         return not_found()
-    
+
 # ERROR HANDLER
 @app.errorhandler(404)
-def not_found(error = None):
+def not_found(error=None):
     response = jsonify({
         'message': "Resource not found: " + request.url,
         "status": 404
